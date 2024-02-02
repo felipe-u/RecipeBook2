2ad04bcc90e14975a10eb79df923d0df5244aa02
felipe-u <felipe_uv@outlook.com>
2024-02-01 19:08:04 -0500

    Building Temp Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  menuOption: string;
+
+  displayOption(option) {
+    this.menuOption = option;
+  }
 }
