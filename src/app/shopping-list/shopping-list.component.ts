eff4427f224841eef1e6b3b5ed411a2428315cbb
felipe-u <felipe_uv@outlook.com>
2024-03-31 19:43:21 -0500

    Improving reactive Service with Observables (Subject)

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
+  private suscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientChanged
+    this.suscription = this.shoppingListService.ingredientChanged
       .subscribe(
         (ingredients: Ingredient[])=> {
           this.ingredients = ingredients;
         }
       )
+  }
+
+  ngOnDestroy(): void {
+    this.suscription.unsubscribe();
   }
 }
